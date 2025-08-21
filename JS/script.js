b057b2eb1fe6fe3de7bc21465d15fc1ee7d3a23c
themanjeetkr <themanjeetkr@gmail.com>
2025-08-21 19:03:36 +0530

    added play button

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,60 @@
+// Global
+let audio = new Audio();
+let currentIndex = 0;
+let songs = [];
+
+// Load and return songs
 async function getsongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
-  let response = await a.text(); // If server returns HTML
+  let response = await a.text();
 
   let div = document.createElement("div");
   div.innerHTML = response;
 
   let as = div.getElementsByTagName("a");
-  let songs = [];
+  let list = [];
 
   for (let i = 0; i < as.length; i++) {
     const element = as[i];
-    if (element.href.endsWith(".mp3")) { 
-      songs.push(element.href);
+    if (element.href.endsWith(".mp3")) {
+      list.push(element.href);
     }
   }
-
-  return songs;
+  return list;
 }
 
+// Play a song by index
+const playmusic = (index) => {
+  if (index < 0 || index >= songs.length) return; // guard
+  currentIndex = index;
+  audio.src = songs[currentIndex];
+  audio.play();
+
+  // change play button icon to pause
+  document.querySelector(".play").src = "images/pause.svg";
+};
+
 async function main() {
-  let songs = await getsongs();
+  songs = await getsongs();
   console.log(songs);
 
-  // Play the first song
-  var audio = new Audio(songs[0]);
+  // set default first song
+  if (songs.length > 0) {
+    audio.src = songs[0];
+  }
 
   // Select UL
   let songul = document.querySelector(".songlist ul");
 
   // Add songs to the list
-  for (let i = 0; i < songs.length; i++) {
+  songs.forEach((song, i) => {
     let li = document.createElement("li");
-    let filename = songs[i].split("/").pop();
+    let filename = song.split("/").pop();
 
-    li.classList.add("invert");
-    li.classList.add("song-item");
+    li.classList.add("invert", "song-item");
 
     li.innerHTML = `
-      <img  src="images/music.svg" alt="music class="invert" />
+      <img src="images/music.svg" alt="music" class="invert" />
       <div class="info invert">
         <div>${filename}</div>
         <div>Unknown Artist</div>
@@ -48,15 +65,43 @@ async function main() {
       </div>
     `;
 
-    // Add click event to playnow only
+    // Play when clicking "Play Now" or whole li
+    li.addEventListener("click", () => playmusic(i));
     li.querySelector(".playnow").addEventListener("click", (e) => {
       e.stopPropagation();
-      audio.src = songs[i];
-      audio.play();
+      playmusic(i);
     });
 
     songul.appendChild(li);
-  }
+  });
+
+  // Controls
+  let playBtn = document.querySelector(".play");
+  let prevBtn = document.querySelector(".previous");
+  let nextBtn = document.querySelector(".next");
+
+  // Play / Pause
+  playBtn.addEventListener("click", () => {
+    if (audio.paused) {
+      audio.play();
+      playBtn.src = "images/pause.svg";
+    } else {
+      audio.pause();
+      playBtn.src = "images/play.svg";
+    }
+  });
+
+  // Previous
+  prevBtn.addEventListener("click", () => {
+    let newIndex = (currentIndex - 1 + songs.length) % songs.length;
+    playmusic(newIndex);
+  });
+
+  // Next
+  nextBtn.addEventListener("click", () => {
+    let newIndex = (currentIndex + 1) % songs.length;
+    playmusic(newIndex);
+  });
 }
 
-main(); // ✅ safely outside the loop
+main();
